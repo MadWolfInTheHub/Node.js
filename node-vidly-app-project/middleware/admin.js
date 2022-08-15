4991444fe40f21c1a805334d80f6a73571ddd6b2
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-08-15 22:46:21 +0800

    test

--- node-vidly-app-project/middleware/admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (req, res, next) {
-  // req.user
-  // 401 Unauthorized
-  // 403 Forbidden
-  console.log(req.user)
-  if(!req.user.isAdmin) return res.status(403).send('Access denied.');
-
-  next();
-}
